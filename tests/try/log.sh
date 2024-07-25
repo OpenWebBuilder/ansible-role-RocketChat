f283cd1c4f4caf7224fd7fc6039dbe064dc1759f
DarkStar⬢🕋🪐☀ <darkstar@blackcubeai.com>
2024-07-24 22:06:41 -0400

    test

---
+++ tests/try/log.sh
@@ -0,0 +1,3 @@
+#To see the log/status of your Rocket.Chat container, execute this command:
+
+docker compose logs -f rocketchat
---
+++ tests/try/restart.sh
@@ -0,0 +1,3 @@
+docker compose stop rocketchat
+docker compose rm rocketchat
+docker compose up -d rocketchat
